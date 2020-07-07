5ce065eec5f5a497585bf6e531f8228674592b87
Nick Capps <nickcapps10@gmail.com>
2020-07-07 20:50:22 +0000

    3. Write query resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -32,7 +32,16 @@ const typeDefs = gql`
     }
 
     type Query {
-        launches: [Launch]!
+        launches(
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+        ): LaunchConnection!
         launch(id: ID!): Launch
         me: User
     }
@@ -41,6 +50,17 @@ const typeDefs = gql`
         bookTrips(launchIds: [ID]!): TripUpdateResponse!
         cancelTrip(launchId: ID!): TripUpdateResponse!
         login(email: String): String # login token
+    }
+
+    """
+    Simple wrapper around our list of launches that contains a cursor to the
+    last item in the list. Pass this cursor to the launches query to fetch results
+    after these.
+    """
+    type LaunchConnection {
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
     }
 
     type TripUpdateResponse {
