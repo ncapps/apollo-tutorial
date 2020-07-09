0c16618980b6c16c507b1a34452a07d52ec03da0
Nick Capps <nickcapps10@gmail.com>
2020-07-09 06:49:01 +0000

    9. Manage local state

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,28 +1,31 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import Pages from './pages';
-import injectStyles from './styles';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import injectStyles from "./styles";
+import { resolvers, typeDefs } from "./resolvers";
 
 const cache = new InMemoryCache();
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: "http://localhost:4000/graphql",
 
     headers: {
-      authorization: localStorage.getItem('token'),
-    }, 
+      authorization: localStorage.getItem("token"),
+    },
   }),
+  typeDefs,
+  resolvers,
 });
 
 cache.writeData({
   data: {
-    isLoggedIn: !!localStorage.getItem('token'),
+    isLoggedIn: !!localStorage.getItem("token"),
     cartItems: [],
   },
 });
@@ -31,6 +34,6 @@ injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Pages />
-  </ApolloProvider>, 
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
